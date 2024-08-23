b747a8919bd8f5b1be1fccdd57ac1512cb2a62a1
pchai <prempreychhay@gmail.com>
2024-08-23 10:09:21 -0700

    Input from keyboard, quick look at variables

---
+++ CPPTutorial/Tutorial0.cpp
@@ -0,0 +1,17 @@
+//// Include a library
+//#include<iostream>
+//
+////using namespace std; // Used to skip typing std::
+//
+////Main Function
+//int main()	// Function head (Return-Type, Name, Parameters)
+//{
+//	// Left to right and down
+//
+//	// Print to the console
+//	std::cout << "Hello World!" << " How are you today?" << std::endl; // "\n" for a new line
+//
+//	system("PAUSE"); // Pause when compile the program
+//
+//	return 0; // End the Program
+//}
---
+++ CPPTutorial/Tutorial1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main()
+{
+	int age = 0; // Variables - can re-assign values but not type
+	int money = 0;
+
+	std::cout << "Please type an age and how much cash money: ";
+	//std::cin >> age; // Get an input
+	//std::cin >> money; // Get an input
+	std::cin >> age >> money; // Get an input
+
+	age = 22; // Initialize age and the last assignment will be output last disregarding the input
+
+	std::cout << "The age is: " << age << "\n" << "Money is: " << money << "\n";
+
+	system("PAUSE");
+
+	return 0;
+}
