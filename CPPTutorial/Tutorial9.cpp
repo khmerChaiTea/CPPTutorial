ffb9571e4825acb90017d52f03724601c32a5fe7
pchai <prempreychhay@gmail.com>
2024-08-26 08:50:13 -0700

    Compound assignment & Increment/Decrement (Operators part 2)

---
+++ CPPTutorial/Tutorial9.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+int main()
+{
+	// Arithmetic Operators (Compound assignment & Increment/Decrement)
+	int i = 20;
+	std::cout << i << "\n";
+	i += 5;
+	std::cout << i << "\n";
+	int k = 10;
+	i += k; // Short way to writing i = i + k
+	std::cout << i << "\n";
+	i -= k;
+	std::cout << i << "\n";
+	i *= k;
+	std::cout << i << "\n";
+	i /= 2;
+	std::cout << i << "\n";
+
+	// ++i = prefix (Use value before change)
+	// i ++ = postfix (Change value before use )
+	std::cout << ++i << "\n";
+	std::cout << i << "\n";
+	std::cout << i++ << "\n";
+	std::cout << i << "\n";
+
+	int x = 3;
+	int y = ++x;
+	std::cout << x << " " << y << "\n";
+
+	x = 3;
+	y = x++;
+	std::cout << x << " " << y << "\n";
+
+	system("PAUSE");
+	return 0;
+}
