b54221757c17965f7121be9161f22a807e60d712
pchai <prempreychhay@gmail.com>
2024-08-26 08:13:00 -0700

    For-loop

--- CPPTutorial/Tutorial6.cpp
+++ CPPTutorial/Tutorial6.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
-
-int main()
-{
-		int choice = 1;
-
-	//// While loop
-	//while(choice <=0)
-	//{
-	//	std::cout << "Quit program? (0: No / 1: Yes)" << "\n";
-	//	std::cin >> choice;
-	//}
-
-	// Do-while loop
-	do
-	{
-		std::cout << "Quit program? (0: No / 1: Yes)" << "\n";
-		std::cin >> choice;
-	}
-	while (choice <= 0);
-
-	system("PAUSE");
-	return 0;
-}
+//#include<iostream>
+//
+//int main()
+//{
+//		int choice = 1;
+//
+//	//// While loop
+//	//while(choice <=0)
+//	//{
+//	//	std::cout << "Quit program? (0: No / 1: Yes)" << "\n";
+//	//	std::cin >> choice;
+//	//}
+//
+//	// Do-while loop
+//	do
+//	{
+//		std::cout << "Quit program? (0: No / 1: Yes)" << "\n";
+//		std::cin >> choice;
+//	}
+//	while (choice <= 0);
+//
+//	system("PAUSE");
+//	return 0;
+//}
---
+++ CPPTutorial/Tutorial7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main()
+{
+	
+	// Keyword (iterator name and start value; condition; iterator increment)
+	for (int i = 0; i < 7; i++)
+	{
+		for (int k = 0; k < 7; k++)
+		{
+			std::cout << i << " " << k << "\n";
+		}
+		std::cout << i << "\n";
+	}
+
+	system("PAUSE");
+	return 0;
+}
