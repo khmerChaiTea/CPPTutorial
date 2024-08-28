93c3b116f1b6b8c5b4ced1be372c9c6a6fc85e6e
pchai <prempreychhay@gmail.com>
2024-08-28 07:39:58 -0700

    Vector container class tutorial and examples of use

--- CPPTutorial/Tutorial13.cpp
+++ CPPTutorial/Tutorial13.cpp
@@ -1,41 +1,41 @@
-#include<iostream>
-#include<string>
-
-int main()
-{
-	const int arrSize = 5;
-	int myArr[arrSize]; // [23][155][65][54][48][486] // 1D array
-
-	//[xx][xx][xx][xx][xx]
-	//[xx][xx][xx][xx][xx]
-	//[xx][xx][xx][xx][xx]
-	//[xx][xx][xx][xx][xx]
-	//[xx][xx][xx][xx][xx]
-
-	int my2DArr[arrSize][arrSize]; // 2D array
-
-	for (size_t i = 0; i < arrSize; i++)
-	{
-		for (size_t k = 0; k < arrSize; k++) // Nested for-loop
-		{
-			my2DArr[i][k] = 0;
-		}
-	}
-
-	my2DArr[0][0] = 10;
-	my2DArr[4][4] = 10;
-	my2DArr[0][4] = 10;
-	my2DArr[4][0] = 10;
-
-	for (size_t i = 0; i < arrSize; i++)
-	{
-		for (size_t k = 0; k < arrSize; k++) // Nested for-loop
-		{
-			std::cout << my2DArr[i][k] << " ";
-		}
-		std::cout << "\n";
-	}
-
-	system("PAUSE");
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//
+//int main()
+//{
+//	const int arrSize = 5;
+//	int myArr[arrSize]; // [23][155][65][54][48][486] // 1D array
+//
+//	//[xx][xx][xx][xx][xx]
+//	//[xx][xx][xx][xx][xx]
+//	//[xx][xx][xx][xx][xx]
+//	//[xx][xx][xx][xx][xx]
+//	//[xx][xx][xx][xx][xx]
+//
+//	int my2DArr[arrSize][arrSize]; // 2D array
+//
+//	for (size_t i = 0; i < arrSize; i++)
+//	{
+//		for (size_t k = 0; k < arrSize; k++) // Nested for-loop
+//		{
+//			my2DArr[i][k] = 0;
+//		}
+//	}
+//
+//	my2DArr[0][0] = 10;
+//	my2DArr[4][4] = 10;
+//	my2DArr[0][4] = 10;
+//	my2DArr[4][0] = 10;
+//
+//	for (size_t i = 0; i < arrSize; i++)
+//	{
+//		for (size_t k = 0; k < arrSize; k++) // Nested for-loop
+//		{
+//			std::cout << my2DArr[i][k] << " ";
+//		}
+//		std::cout << "\n";
+//	}
+//
+//	system("PAUSE");
+//	return 0;
+//}
---
+++ CPPTutorial/Tutorial14.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+int main()
+{
+	std::vector<int> myIntVec;
+
+	myIntVec.push_back(23);
+	myIntVec.push_back(4);
+	myIntVec.push_back(453);
+	myIntVec.push_back(844);
+
+	std::vector<std::string> myStrVec;
+
+	myStrVec.push_back("Prem");
+	myStrVec.push_back("John");
+	myStrVec.push_back("Luke");
+
+	myStrVec.erase(myStrVec.begin() + 1); // Begin is the start of the iterator of a loop
+	 // Erase the end; but since the end since is exclusive use "myStrVec.end() - 1"
+	//myStrVec.erase(myStrVec.end() - 1);
+
+	//myStrVec.clear(); // Cleared the vector / array
+
+	// Removed a certain elementat an unknown position if it existed
+	//bool erased = false;
+	//for (size_t i = 0; i < myIntVec.size() && erased == false; i++)
+	//{
+	//	myIntVec.erase(myIntVec.begin() + i);
+	//	erased = true;
+	//}
+
+	int integerToRemove = 453;
+	bool found = false;
+
+	for (size_t i = 0; i < myIntVec.size() && found == false; i++)
+	{
+		if (myIntVec[i] == integerToRemove)
+		{
+			myIntVec.erase(myIntVec.begin() + i);
+			found = true;
+			std::cout << integerToRemove << " has been erased!" << "\n";
+		}
+	}
+
+	if (found == false)
+		std::cout << integerToRemove << " was not found in myIntArr!" << "\n";
+
+	std::cout << "\n";
+
+	for (size_t i = 0; i < myIntVec.size(); i++)
+	{
+		std::cout << myIntVec[i] << " ";
+	}
+
+	std::cout << "\n";
+
+	for (size_t i = 0; i < myStrVec.size(); i++)
+	{
+		std::cout << myStrVec[i] << " ";
+	}
+
+	std::cout << "\n";
+
+	std::cout << "Size of myIntVec: " << myIntVec.size() << "\n";
+	std::cout << "Size of myStrVec: " << myStrVec.size() << "\n";
+
+	system("Pause");
+	return 0;
+}
