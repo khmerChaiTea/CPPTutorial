7f86b0b6d5cab40afbb39dcf27256fa78d79a9f9
pchai <prempreychhay@gmail.com>
2024-08-29 07:51:54 -0700

    Functions and their uses (PART 1)

--- CPPTutorial/Tutorial15.cpp
+++ CPPTutorial/Tutorial15.cpp
@@ -1,25 +1,24 @@
-#include<iostream>
-#include<string>
-
-int main()
-{
-	int i = 20;
-	int k = 30;
-	double d = 20.3;
-
-	// Type, reference operator, initial value!
-	int& r = i;
-	//r = k;
-	double& dr = d;
-
-	r = k;
-	r = 10;
-	std::cout << i << "\n";
-	std::cout << k << "\n";
-	std::cout << r << "\n";
-	std::cout << dr << "\n";
-	std::cout << INT_MAX << "\n";
-
-	system("PAUSE");
-	return 0;
-}
+//#include<iostream>
+//
+//int main()
+//{
+//	int i = 20;
+//	int k = 30;
+//	double d = 20.3;
+//
+//	// Type, reference operator, initial value!
+//	int& r = i;
+//	//r = k;
+//	double& dr = d;
+//
+//	r = k;
+//	r = 10;
+//	std::cout << i << "\n";
+//	std::cout << k << "\n";
+//	std::cout << r << "\n";
+//	std::cout << dr << "\n";
+//	std::cout << INT_MAX << "\n";
+//
+//	system("PAUSE");
+//	return 0;
+//}
---
+++ CPPTutorial/Tutorial16.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+
+// Return type, name, input parameter(s)
+int addTwoNumbers(int a, int b) // Function header
+{
+	std::cout << "a: " << a << " " << " b: " << b << "\n";
+
+	return a + b;
+}
+
+void printResult(int a, int b, int c)
+{
+	int sum = a + b + c;
+	std::cout << "sum: " << sum << "\n";
+}
+
+void hello()
+{
+	std::cout << "Hello World from Function!" << "\n";
+}
+
+int main()
+{
+	std::cout << addTwoNumbers(5, 7) << "\n";
+	std::cout << addTwoNumbers(15, 91) << "\n";
+
+	int sum = addTwoNumbers(165, 61);
+	std::cout << sum << "\n";
+
+	printResult(1, 6, 9);
+	printResult(4, 27, 49);
+
+	int a = 15;
+	int b = 46;
+	int c = 31;
+
+	std::cout << addTwoNumbers(a, b) << "\n";
+	printResult(a, b, c);
+
+	hello();
+
+	system("PAUSE");
+	return 0;
+}
