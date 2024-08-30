4f53b4a624e8b9590b85752984862c2303ca0e45
pchai <prempreychhay@gmail.com>
2024-08-30 09:39:00 -0700

    Functions in different files! (PART 4)

---
+++ CPPTutorial/myMathFunctions.h
@@ -0,0 +1,22 @@
+#pragma once
+/*
+* #ifndef MYMATHFUNCTIONS_H // Key1
+* #define MYMATHFUNCTIONS_H
+* 
+* #endif
+*/
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int subtraction(int a, int b)
+{
+	return a - b;
+}
+
+int multiplication(int a, int b)
+{
+	return a * b;
+}
