051de753511db1066d203225b8fcdfe77deb7da1
pchai <prempreychhay@gmail.com>
2024-08-30 09:12:16 -0700

    Function prototypes... How to divide a function into parts (PART 3)

--- CPPTutorial/Tutorial17.cpp
+++ CPPTutorial/Tutorial17.cpp
@@ -1,73 +1,73 @@
-#include<iostream>
-#include<string>
-
-// Reference parameters
-// Pass by value
-// Pass by reference
-
-//double function(int a, float b, std::string name)
+//#include<iostream>
+//#include<string>
+//
+//// Reference parameters
+//// Pass by value
+//// Pass by reference
+//
+////double function(int a, float b, std::string name)
+////{
+////	return a + b;
+////}
+//
+//void changeNameOfCharacter_VAL(std::string name) //  Pass by value
 //{
-//	return a + b;
+//	/*
+//		Funcction will change the "name" variable (By Value).
+//	*/
+//
+//	name = "DANIEL";
 //}
-
-void changeNameOfCharacter_VAL(std::string name) //  Pass by value
-{
-	/*
-		Funcction will change the "name" variable (By Value).
-	*/
-
-	name = "DANIEL";
-}
-
-void changeNameOfCharacter_REF(std::string &name_to_change, std::string name) //  Pass by reference
-{
-	/*
-		Funcction will change the "name" variable (By reference).
-	*/
-
-	name_to_change = name;
-}
-
-void changeNameOfCharacter_P(std::string *name) //  Pass by reference (pointer)
-{
-	/*
-		Funcction will change the "name" variable (By reference - pointer).
-	*/
-
-	*name = "Daniel";
-}
-
-void incrementEXP(int& exp, int level)
-{
-	exp += 20 + level * 2 - 3;
-}
-
-int main()
-{
-	int level = 1;
-	int exp = 0;
-
-	std::cout << exp << "\n";
-
-	incrementEXP(exp, level);
-	std::cout << exp << "\n";
-
-	std::string characterName = "John";
-	std::string myName = "Erika";
-
-	changeNameOfCharacter_VAL(characterName);
-	std::cout << characterName << "\n";
-
-	changeNameOfCharacter_REF(characterName, "Erik");
-	std::cout << characterName << "\n";
-	changeNameOfCharacter_REF(myName, "Peter");
-	std::cout << myName << "\n";
-	changeNameOfCharacter_REF(myName, characterName);
-	std::cout << myName << "\n";
-
-	changeNameOfCharacter_P(&characterName);
-	std::cout << characterName << "\n";
-
-	system("PAUSE");
-	return 0;
-}
+//
+//void changeNameOfCharacter_REF(std::string &name_to_change, std::string name) //  Pass by reference
+//{
+//	/*
+//		Funcction will change the "name" variable (By reference).
+//	*/
+//
+//	name_to_change = name;
+//}
+//
+//void changeNameOfCharacter_P(std::string *name) //  Pass by reference (pointer)
+//{
+//	/*
+//		Funcction will change the "name" variable (By reference - pointer).
+//	*/
+//
+//	*name = "Daniel";
+//}
+//
+//void incrementEXP(int& exp, int level)
+//{
+//	exp += 20 + level * 2 - 3;
+//}
+//
+//int main()
+//{
+//	int level = 1;
+//	int exp = 0;
+//
+//	std::cout << exp << "\n";
+//
+//	incrementEXP(exp, level);
+//	std::cout << exp << "\n";
+//
+//	std::string characterName = "John";
+//	std::string myName = "Erika";
+//
+//	changeNameOfCharacter_VAL(characterName);
+//	std::cout << characterName << "\n";
+//
+//	changeNameOfCharacter_REF(characterName, "Erik");
+//	std::cout << characterName << "\n";
+//	changeNameOfCharacter_REF(myName, "Peter");
+//	std::cout << myName << "\n";
+//	changeNameOfCharacter_REF(myName, characterName);
+//	std::cout << myName << "\n";
+//
+//	changeNameOfCharacter_P(&characterName);
+//	std::cout << characterName << "\n";
+//
+//	system("PAUSE");
+//	return 0;
+//}
---
+++ CPPTutorial/Tutorial18.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+
+// Function Prototype
+// Function Header: Return-type, Name, input parameters
+
+void myFunc1(); // Prototype
+void myFunc2(); // Prototype
+float myDivisionFunc(float, float); // Prototype
+
+int main()
+{
+	myFunc1();
+	myFunc2();
+
+	std::cout << myDivisionFunc(10.f, 2.f) << "\n";
+
+	system("PAUSE");
+	return 0;
+}
+
+void myFunc1() // Definition
+{
+	std::cout << "Hello from MyFunc1" << "\n";
+}
+
+void myFunc2() // Definition
+{
+	std::cout << "Hello from MyFunc2" << "\n";
+}
+
+float myDivisionFunc(float a, float b) // Definition
+{
+	if (b == 0)
+	{
+		std::cout << "Div by 0! Illegal Operation!" << "\n";
+		return 0.f; // Exit Function
+	}
+
+	return a / b; // Exit Function
+}
