98edc6d9156b99c7efb8c41ee3e906deb528ae71
pchai <prempreychhay@gmail.com>
2024-09-01 07:02:45 -0700

    Scope tutorial. What is a scope and why is it important?

--- CPPTutorial/Tutorial21.cpp
+++ CPPTutorial/Tutorial21.cpp
@@ -1,29 +1,29 @@
-#include<iostream>
-
-// Does a vaule need to be changed? If not; make it const, else just make it a reference.
-void funct1(int& valueTochange, const int& value)
-{
-	/* Get a variable as a reference and replace its value
-	with another value */
-
-	valueTochange = value;
-}
-
-const float& median(const int& a, const int b, const int c)
-{
-	return (a + b + c) / 3.f;
-}
-
-int main()
-{
-	int a = 20;
-	int b = 10;
-
-	funct1(a, b);
-
-	std::cout << a << "\n";
-	std::cout << median(10, 20, 30) << "\n";
-
-	system("PAUSE");
-	return 0;
-}
+//#include<iostream>
+//
+//// Does a vaule need to be changed? If not; make it const, else just make it a reference.
+//void funct1(int& valueTochange, const int& value)
+//{
+//	/* Get a variable as a reference and replace its value
+//	with another value */
+//
+//	valueTochange = value;
+//}
+//
+//const float& median(const int& a, const int b, const int c)
+//{
+//	return (a + b + c) / 3.f;
+//}
+//
+//int main()
+//{
+//	int a = 20;
+//	int b = 10;
+//
+//	funct1(a, b);
+//
+//	std::cout << a << "\n";
+//	std::cout << median(10, 20, 30) << "\n";
+//
+//	system("PAUSE");
+//	return 0;
+//}
