8855743ee32b4d56618b55dc83d937d859d1152b
pchai <prempreychhay@gmail.com>
2024-09-04 08:17:16 -0700

    Class initialization list

--- CPPTutorial/Tutorial27.cpp
+++ CPPTutorial/Tutorial27.cpp
@@ -1,71 +1,71 @@
-#include<iostream>
-#include<string>
-#include<vector>
-
-class MyClass
-{
-private:
-	// variable
-	std::vector<std::string> myStrings;
-
-public:
-	MyClass();
-
-	// Accessors
-	const std::vector<std::string>& getMyStrings() const;
-
-	// Modifiers
-
-
-	// Functions
-	void add(const std::string str);
-	const std::string toString() const;
-};
-
-// MyClass Function Definitions
-MyClass::MyClass() // Constructor
-{
-	this->myStrings.push_back("Hi I am first!");
-}
-
-const std::vector<std::string>& MyClass::getMyStrings() const
-{
-	return this->myStrings;
-}
-
-void MyClass::add(const std::string str)
-{
-	this->myStrings.push_back(str);
-}
-
-const std::string MyClass::toString() const
-{
-	std::string str = "";
-	for (size_t i = 0; i < this->myStrings.size(); i++)
-	{
-		str += std::to_string(i) + ": " + this->myStrings[i] + "\n";
-	}
-	return str;
-}
-
-int main()
-{
-	MyClass obj;
-
-	obj.add("Hi I am another string from main!");
-	
-	for (size_t i = 0; i < 10; i++)
-	{
-		obj.add("Hi! I am number: " + std::to_string(i) + " for this loop.");
-	}
-
-	std::cout << obj.toString() << "\n";
-
-	//for (size_t i = 0; i < obj.getMyStrings().size(); i++)
-	//{
-	//	std::cout << obj.getMyStrings().at(i) << "\n";
-	//}
-
-	system("PAUSE");
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//
+//class MyClass
+//{
+//private:
+//	// variable
+//	std::vector<std::string> myStrings;
+//
+//public:
+//	MyClass();
+//
+//	// Accessors
+//	const std::vector<std::string>& getMyStrings() const;
+//
+//	// Modifiers
+//
+//
+//	// Functions
+//	void add(const std::string str);
+//	const std::string toString() const;
+//};
+//
+//// MyClass Function Definitions
+//MyClass::MyClass() // Constructor
+//{
+//	this->myStrings.push_back("Hi I am first!");
+//}
+//
+//const std::vector<std::string>& MyClass::getMyStrings() const
+//{
+//	return this->myStrings;
+//}
+//
+//void MyClass::add(const std::string str)
+//{
+//	this->myStrings.push_back(str);
+//}
+//
+//const std::string MyClass::toString() const
+//{
+//	std::string str = "";
+//	for (size_t i = 0; i < this->myStrings.size(); i++)
+//	{
+//		str += std::to_string(i) + ": " + this->myStrings[i] + "\n";
+//	}
+//	return str;
+//}
+//
+//int main()
+//{
+//	MyClass obj;
+//
+//	obj.add("Hi I am another string from main!");
+//	
+//	for (size_t i = 0; i < 10; i++)
+//	{
+//		obj.add("Hi! I am number: " + std::to_string(i) + " for this loop.");
+//	}
+//
+//	std::cout << obj.toString() << "\n";
+//
+//	//for (size_t i = 0; i < obj.getMyStrings().size(); i++)
+//	//{
+//	//	std::cout << obj.getMyStrings().at(i) << "\n";
+//	//}
+//
+//	system("PAUSE");
+//	return 0;
+//}
---
+++ CPPTutorial/Tutorial28.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<ctime>
+
+class Dice
+{
+private:
+	const int value;
+
+public:
+	Dice(int value);
+	virtual ~Dice();
+
+	// Accessors
+	const int& getValue() const;
+
+	// Functions
+	const int toss() const;
+};
+
+int main()
+{
+	srand(static_cast<unsigned>(time(0))); // srand(time(static_cast<unsigned>(0))); throw error
+
+	Dice d6(6);
+	Dice d12(12);
+
+	for (size_t i = 0; i < 6; i++)
+	{
+		std::cout << d6.toss() << "\n";
+	}
+
+	std::cout << "\n";
+
+	for (size_t i = 0; i < 12; i++)
+	{
+		std::cout << d12.toss() << "\n";
+	}
+
+	system("PAUSE");
+	return 0;
+}
+
+// Class dice
+Dice::Dice(int value)
+	: value(value) // Initializer list for class constant member variables and references
+{
+	
+}
+
+Dice::~Dice()
+{
+
+}
+
+const int& Dice::getValue() const
+{
+	return this->value;
+}
+
+const int Dice::toss() const
+{
+	return rand()%this->value + 1;
+}
