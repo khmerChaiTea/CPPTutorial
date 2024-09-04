e36a84acf0ccb763a1f3b4cb11fe77e7909d7869
pchai <prempreychhay@gmail.com>
2024-09-04 14:45:51 -0700

    Input validation! (cin wrong input error-check)

--- CPPTutorial/Tutorial29.cpp
+++ CPPTutorial/Tutorial29.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
-#include<ctime>
-#include "Dice.h"
-
-int main()
-{
-	srand(static_cast<unsigned>(time(0)));
-
-	Dice d6;
-	Dice d12(12, "Red");
-
-	std::cout << d6.toss() << " Color: " << d6.getColor() << "\n";
-	std::cout << d12.toss() << " Color: " << d12.getColor() << "\n";
-
-	system("PAUSE");
-	return 0;
-}
+//#include<iostream>
+//#include<ctime>
+//#include "Dice.h"
+//
+//int main()
+//{
+//	srand(static_cast<unsigned>(time(0)));
+//
+//	Dice d6;
+//	Dice d12(12, "Red");
+//
+//	std::cout << d6.toss() << " Color: " << d6.getColor() << "\n";
+//	std::cout << d12.toss() << " Color: " << d12.getColor() << "\n";
+//
+//	system("PAUSE");
+//	return 0;
+//}
