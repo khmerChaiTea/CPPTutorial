e9c506cff3ce1a5cb66eb72c3906ab05866e6dfe
pchai <prempreychhay@gmail.com>
2024-09-04 15:07:16 -0700

    Classes in classes

--- CPPTutorial/Tutorial30.cpp
+++ CPPTutorial/Tutorial30.cpp
@@ -1,74 +1,74 @@
-#include<iostream>
-#include <iomanip>
-
-template <typename T>
-void getChoice(T& choice)
-{
-	//int choice = 0;
-
-	std::cout << "- Enter choice: ";
-	std::cin >> std::setw(1) >> choice;
-
-	// Error check -- while not good
-	while (!std::cin.good())
-	{
-		// Report problem
-		std::cout << "ERROR: Faulty input! Try again..." << "\n";
-
-		// Clear stream
-		std::cin.clear();
-		std::cin.ignore(INT_MAX, '\n');
-
-		// Get input again...
-		std::cout << "- Enter choice: ";
-		std::cin >> std::setw(1) >> choice;
-	}
-
-	// Clear stream
-	std::cin.clear();
-	std::cin.ignore(INT_MAX, '\n');
-
-	//return choice;
-}
-
-int main()
-{
-	int choice = -1;
-	//char choiceChar = 'n';
-
-	//std::cout << "- Enter choice: ";
-	//std::cin >> choice;
-	getChoice(choice);
-
-	while (choice > 0) // Menu
-	{
-		switch (choice)
-		{
-		case 1:
-			std::cout << "Option ONE" << "\n";
-			break;
-
-		case 2:
-			std::cout << "Option TWO" << "\n";
-			break;
-
-		case 3:
-			std::cout << "Option THREE" << "\n";
-			break;
-
-
-		default:
-			std::cout << "No such Option!" << "\n";
-			break;
-		}
-
-		//std::cout << "- Enter choice: ";
-		//std::cin >> choice;
-		getChoice(choice);
-	}
-
-	std::cout << "Quitting..." << "\n";
-
-	system("PAUSE");
-	return 0;
-}
+//#include<iostream>
+//#include <iomanip>
+//
+//template <typename T>
+//void getChoice(T& choice)
+//{
+//	//int choice = 0;
+//
+//	std::cout << "- Enter choice: ";
+//	std::cin >> std::setw(1) >> choice;
+//
+//	// Error check -- while not good
+//	while (!std::cin.good())
+//	{
+//		// Report problem
+//		std::cout << "ERROR: Faulty input! Try again..." << "\n";
+//
+//		// Clear stream
+//		std::cin.clear();
+//		std::cin.ignore(INT_MAX, '\n');
+//
+//		// Get input again...
+//		std::cout << "- Enter choice: ";
+//		std::cin >> std::setw(1) >> choice;
+//	}
+//
+//	// Clear stream
+//	std::cin.clear();
+//	std::cin.ignore(INT_MAX, '\n');
+//
+//	//return choice;
+//}
+//
+//int main()
+//{
+//	int choice = -1;
+//	//char choiceChar = 'n';
+//
+//	//std::cout << "- Enter choice: ";
+//	//std::cin >> choice;
+//	getChoice(choice);
+//
+//	while (choice > 0) // Menu
+//	{
+//		switch (choice)
+//		{
+//		case 1:
+//			std::cout << "Option ONE" << "\n";
+//			break;
+//
+//		case 2:
+//			std::cout << "Option TWO" << "\n";
+//			break;
+//
+//		case 3:
+//			std::cout << "Option THREE" << "\n";
+//			break;
+//
+//
+//		default:
+//			std::cout << "No such Option!" << "\n";
+//			break;
+//		}
+//
+//		//std::cout << "- Enter choice: ";
+//		//std::cin >> choice;
+//		getChoice(choice);
+//	}
+//
+//	std::cout << "Quitting..." << "\n";
+//
+//	system("PAUSE");
+//	return 0;
+//}
---
+++ CPPTutorial/Tutorial31.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+
+class Car
+{
+private:
+	class Wheel
+	{
+	//private: // Not needed when within another class
+
+	public:
+		int pressure;
+
+		Wheel(int pressure) { this->pressure = pressure; };
+		~Wheel() {};
+	};
+
+	Wheel tl;
+	Wheel tr;
+	Wheel bl;
+	Wheel br;
+
+public:
+	Car() :
+		tl(1), tr(2), bl(3), br(4)
+	{
+
+	};
+	~Car() {};
+
+	void printWheels()
+	{
+		std::cout << "Wheels: \n"
+			<< "Top Left: " << tl.pressure << "\n"
+			<< "Top Right: " << tr.pressure << "\n"
+			<< "Bottom Left: " << bl.pressure << "\n"
+			<< "Bottom right: " << br.pressure << "\n"
+			<< "\n";
+	}
+};
+
+int main()
+{
+	Car car1;
+
+	car1.printWheels();
+
+	system("pause");
+	return 0;
+}
