d9adcf8993fda15ebc5c6aa132903925da30b125
pchai <prempreychhay@gmail.com>
2024-09-04 19:56:15 -0700

    Class copy constructor

--- CPPTutorial/Tutorial35.cpp
+++ CPPTutorial/Tutorial35.cpp
@@ -1,39 +1,39 @@
-/*
-* Tutorial: Deep copy vs. Shallow copy
-*/
-
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-int main()
-{
-	int num1 = 10;
-	int num2 = 20;
-
-	//int* intptr = &num1;
-	//int* intptrd = new int(30); // Can cause memory leak -- need to be delete after created/used
-
-	//intptrd = intptr; // Create memory leak -- shallow copy
-	//intptr = intptrd;
-
-	int* intptr1 = new int(50);
-	int* intptr2 = new int(30);
-
-	// Shallow copy
-	//intptr1 = intptr2; // Create memory leak!
-
-	// Deep copy (safe!)
-	*intptr1 = *intptr2;
-
-	num1 = num2;
-
-	cout << num1 << "\n";
-
-	delete intptr1; // Delete to prevent memory leak --  cleaning up
-	delete intptr2;
-
-	system("pause");
-	return 0;
-}
+///*
+//* Tutorial: Deep copy vs. Shallow copy
+//*/
+//
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//
+//	//int* intptr = &num1;
+//	//int* intptrd = new int(30); // Can cause memory leak -- need to be delete after created/used
+//
+//	//intptrd = intptr; // Create memory leak -- shallow copy
+//	//intptr = intptrd;
+//
+//	int* intptr1 = new int(50);
+//	int* intptr2 = new int(30);
+//
+//	// Shallow copy
+//	//intptr1 = intptr2; // Create memory leak!
+//
+//	// Deep copy (safe!)
+//	*intptr1 = *intptr2;
+//
+//	num1 = num2;
+//
+//	cout << num1 << "\n";
+//
+//	delete intptr1; // Delete to prevent memory leak --  cleaning up
+//	delete intptr2;
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ CPPTutorial/Tutorial36.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+#include<ctime>
+
+class Car
+{
+private:
+	std::string name;
+	int* horsePower;
+
+public:
+	Car(std::string name, int horsePower);
+	Car(const Car& obj); // Copy constructor
+	~Car();
+
+	const std::string& getName() const;
+	const int& getHorsePower() const;
+};
+
+
+
+int main()
+{
+	Car car1("Audi A4", 2000);
+	//Car car2(car1);  // Same as Car car2 = car1;
+	Car car2 = car1; // Copy constructor just as Car car2(car1);
+
+	//car2 = car1; // Assignment operator
+
+	std::cout << car1.getName() << " " << car1.getHorsePower() << "\n";
+	std::cout << car2.getName() << " " << car2.getHorsePower() << "\n";
+
+	system("pause");
+	return 0;
+}
+
+Car::Car(std::string name, int horsePower)
+{
+	this->name = name;
+	this->horsePower = new int(horsePower);
+}
+
+Car::Car(const Car& obj)
+{
+	this->name = obj.name;
+	this->horsePower = new int(*obj.horsePower);
+}
+
+Car::~Car()
+{
+	delete this->horsePower;
+}
+
+const std::string& Car::getName() const
+{
+	return this->name;
+}
+
+const int& Car::getHorsePower() const
+{
+	return *this->horsePower;
+}
