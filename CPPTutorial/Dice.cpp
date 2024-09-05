9b235f7f72769ae595a3c23b8c2b241e824c248e
pchai <prempreychhay@gmail.com>
2024-09-04 19:08:56 -0700

    Static variables and functions, in and outside classes (Useful stuff!)

--- CPPTutorial/Dice.cpp
+++ CPPTutorial/Dice.cpp
@@ -1,10 +1,19 @@
 #include "Dice.h"
 
+int Dice::ID = 0;
+
+// Static functions
+int Dice::getStaticID()
+{
+	return Dice::ID;
+}
+
 // Do not define the varible here if it is already defined/initialized
 Dice::Dice(const int value, const std::string color)
 	: value(value)
 {
 	this->color = color;
+	Dice::ID++;
 }
 
 Dice::~Dice()
--- CPPTutorial/Tutorial33.cpp
+++ CPPTutorial/Tutorial33.cpp
@@ -1,40 +1,40 @@
-#include<iostream>
-#include<ctime>
-#include<vector>
-
-int main()
-{
-	// Initialize
-	std::vector<int> intArr(1000000, 1);
-	int sum = 0;
-
-	// Optimization of loops
-	int start_t = clock();
-
-	// Normal loop (avg: 0.002 ms)
-	//for (size_t i = 0; i < intArr.size(); i++)
-	//{
-	//	sum += intArr[i];
-	//}
-
-	// C++ 11 foreach loop (0.001? keep saying 0 for me)
-	//for (auto &i : intArr)
-	//	sum += i;
-
-	// Iterator range loop (0.02)
-	//const std::vector<int>::const_iterator end = cend(intArr); // Iterator caching
-	//for (std::vector<int>::const_iterator it = cbegin(intArr); it != end; it++)
-	const auto end = cend(intArr); // Iterator caching
-	for (auto it = cbegin(intArr); it != end; it++)
-	{
-		sum += *it;
-	}
-
-	int end_t = clock();
-
-	std::cout << "Time: " << (end_t - start_t) / double(CLOCKS_PER_SEC) << "\n";
-	std::cout << "Sum: " << sum << "\n";
-
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//#include<ctime>
+//#include<vector>
+//
+//int main()
+//{
+//	// Initialize
+//	std::vector<int> intArr(1000000, 1);
+//	int sum = 0;
+//
+//	// Optimization of loops
+//	int start_t = clock();
+//
+//	// Normal loop (avg: 0.002 ms)
+//	//for (size_t i = 0; i < intArr.size(); i++)
+//	//{
+//	//	sum += intArr[i];
+//	//}
+//
+//	// C++ 11 foreach loop (0.001? keep saying 0 for me)
+//	//for (auto &i : intArr)
+//	//	sum += i;
+//
+//	// Iterator range loop (0.02)
+//	//const std::vector<int>::const_iterator end = cend(intArr); // Iterator caching
+//	//for (std::vector<int>::const_iterator it = cbegin(intArr); it != end; it++)
+//	const auto end = cend(intArr); // Iterator caching
+//	for (auto it = cbegin(intArr); it != end; it++)
+//	{
+//		sum += *it;
+//	}
+//
+//	int end_t = clock();
+//
+//	std::cout << "Time: " << (end_t - start_t) / double(CLOCKS_PER_SEC) << "\n";
+//	std::cout << "Sum: " << sum << "\n";
+//
+//	system("pause");
+//	return 0;
+//}
